464eca9c770be76c066b67d6a8e4abacf7f1395d
Carina Sauter <carina.sauter@berkeley.edu>
2018-04-04 02:47:04 -0700

    cleanup ;

--- app/__init__.py
+++ app/__init__.py
@@ -9,25 +9,20 @@ eventlet.monkey_patch()
 
 app = Flask(__name__)
 
+app.config.from_object('config')
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 async_mode = 'eventlet'
 socketio = SocketIO(app, async_mode=async_mode, logger=True, engineio_logger=True)
 
-app.config['SECRET'] = 'my secret key'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'
+# app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = 'io.adafruit.com'
 app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = ''
-app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_USERNAME'] = 'caycay'
+app.config['MQTT_PASSWORD'] = '80009f64496041f79d5f440181eeb727'
 app.config['MQTT_KEEPALIVE'] = 5
 app.config['MQTT_TLS_ENABLED'] = False
-
-
-app.config.from_object('config')
-
-
 
 
 mqtt = Mqtt(app)
